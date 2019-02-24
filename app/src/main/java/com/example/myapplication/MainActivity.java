5b2105345ac01ffda3e71d5ab7ba5be4f92c677a
pfpalmer <pfpalmer@gmail.com>
2019-02-24 15:33:40 -0600

    test

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,15 +2,18 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button rollButtonId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        val rollButton = findViewById<Button>()
+        Button rollButtonID = (Button)findViewById(R.id.rollButton);
     }
 }
